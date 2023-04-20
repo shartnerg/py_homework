ae6f6ca29d3a1c2bd555a77db77f2e8046099b2d
George Shartner <shartnerg@gmail.com>
2023-04-20 22:02:27 +0300

    hw1

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,1 +1,5 @@
-print("hello world")
+a= int(input("Vvedite trehznachnoe chislo: "))
+if a/100>=1 and a/100<10:
+    print(int(a/100+((a/10)%10)+a%10))
+else:
+    print("Eto ne trehznachnoe chislo")
