1db82190c1e56c4cffee654b0724b18aa0bd53bb
George Shartner <shartnerg@gmail.com>
2023-07-04 18:27:35 +0300

    hw7_2_36

---
+++ hw7/hw2_36.py
@@ -0,0 +1,9 @@
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for row in range(1, num_rows + 1):
+        for column in range(1, num_columns + 1):
+            result = operation(row, column)
+            print("{:4d}".format(result), end=' ')
+        print()
+
+
+print_operation_table(lambda x, y: x * y)
